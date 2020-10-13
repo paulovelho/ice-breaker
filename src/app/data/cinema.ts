5150fda274109e3b7df6318de857112a4486e306
Paulo Martins <paulo.martins@vokal.io>
2020-10-12 22:19:44 -0300

    adding categories data

---
+++ src/app/data/cinema.ts
@@ -0,0 +1,18 @@
+export const data = {
+	version: 1.0,
+	name: "cinema",
+	data: [
+"Qual o seu filme favorito?",
+"Qual filme é o seu maior guilty pleasure?",
+"Qual seu gênero cinematográfico favorito?",
+"Qual era seu filme favorito quando criança?",
+"Qual filme você acha que todo mundo deveria assistir?",
+"Se fizessem um filme da sua vida, como você gostaria que fosse o pôster?",
+"Se a sua vida fosse um filme, qual seria o gênero?",
+"Se a sua vida fosse um filme, que ator te interpretaria?",
+"Se a sua vida fosse um filme, quem seria o diretor?",
+"Cinema ou teatro?",
+"Quem merecia um Oscar que ainda não ganhou?",
+"Qual ator/atriz te faz ir ao cinema?",
+]
+}
