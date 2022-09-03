73d80feb2d79d7219cc1a78f7d71496a7813a472
Paulo Velho <paulovelho@paulovelho.com>
2022-09-03 15:37:37 -0300

    adding questions

---
+++ src/app/data/rotulos.ts
@@ -0,0 +1,15 @@
+export const data = {
+	version: 1.0,
+	name: "rotulos",
+	title: "Rótulos",
+	data: [
+"Vai votar em quem no primeiro turno?",
+"Qual a sobremesa favorita?",
+"Qual o filme favorito?",
+"Que tipos de livro lê?",
+"Tem ficha limpa?",
+"Mora em qual bairro?",
+"Qual o carro?",
+"Trabalha com o quê?",
+]
+}
